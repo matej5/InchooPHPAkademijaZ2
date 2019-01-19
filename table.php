6e94f72f42be7a90c4daf24513b2c61a451285b0
Matej <mstajduhar55@gmail.com>
2019-01-19 08:59:14 +0100

    Created logic for bolding first num after average

--- table.php
+++ table.php
@@ -11,16 +11,41 @@
     <!-- border-colapse used so <td> won't have spaces  -->
     <table style="border-collapse: collapse;">
         <?php
+        //after first larger num than average change to false
+        $firstAfterAvg = true;
+        //get string combination with num and ','
+        $string = $_POST['num'];
+        //store numbers from string into array seperated by coma
+        $array = explode(',', $string);
+
+        //2 for loops for to create grid 16 x 16
         for($a = 0; $a <16; $a++) {
             echo '<tr>';
             for ($b = 0; $b < 16; $b++) {
-                echo '<td style="border: 1px solid;">';
+                echo '<td style="border: 1px solid; width: 20px; height: 20px;">';
+                    foreach ($array as $i) {
+                        //check if num of field is equal to num in array
+                        if ($a * 16 + $b + 1 == $i && $i%2 == 0) {
+                            if($firstAfterAvg && $i > array_sum($array)/count($array)){
+                                echo '<b>',$i,'</b>';
+                                $firstAfterAvg = false;
+                                break;
+                            }
 
+                            echo $i;
+                            //if there are multple num of same values we need to echo only
+                            break;
+                        }
+                    }
                 echo '</td>';
             }
             echo '</tr>';
         }
         ?>
     </table>
+    <hr>
+    <?php
+    echo 'Average: ',array_sum($array)/count($array);
+    ?>
 </body>
 </html>
