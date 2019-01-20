a02c79ac5f237dca8e5498dc88650ca0fc74a3cd
matej5 <mstajduhar55@gmail.com>
2019-01-20 18:05:59 +0100

    moved styles to css file

--- table.php
+++ table.php
@@ -5,11 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Matej Štajduhar</title>
 </head>
 <body>
     <!-- border-colapse used so <td> won't have spaces  -->
-    <table style="border-collapse: collapse;">
+    <table>
         <?php
         //if directly opened table.php it will redirect to index.html
         if(!isset($_POST['num'])){
@@ -42,7 +43,7 @@
                 for ($b = 0; $b < $size; $b++) {
 
                     //using max inside sqrt to get width and hight needed
-                    echo '<td style="border: 1px solid; width: 20px; height: 20px;">';
+                    echo '<td>';
                     foreach ($array as $i) {
 
                         //check if num of field is equal to num in array
